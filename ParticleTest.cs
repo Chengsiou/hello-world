6e7b85fd07a1a6072859c5cf3a7b38b6c1bd9aee
Chengsiou <a0915655212@gmail.com>
2016-02-23 17:39:03 +0800

    Added files via upload

---
+++ ParticleTest.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        this.gameObject.particleSystem.startLifetime = 10; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
